6a0dc9de0a5fe43cfcf446c95a7957d5c7f0467d
fatimajavid <fatimajavid101@gmail.com>
2021-09-16 18:36:36 -0400

    Flixster Part 2

--- app/src/main/java/com/example/flixsterunit1/models/Movie.java
+++ app/src/main/java/com/example/flixsterunit1/models/Movie.java
@@ -3,22 +3,32 @@ package com.example.flixsterunit1.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
 
+    int movieID;
     String backdropPath;
     String posterPath;
     String title;
     String overview;
+    double rating;
+
+
+    //Empty conductor needed by parceler library
+    public Movie() {}
 
     public Movie(JSONObject jsonObject) throws JSONException {
         backdropPath = jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieID = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -43,6 +53,14 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public double getRating(){
+        return rating;
+    }
+
+    public int getMovieID(){
+        return movieID;
     }
 }
 
